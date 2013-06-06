e96dd5360759be6ba9916cdc3febde71c9d9da19
Steve Faulkner <southpolesteve@gmail.com>
2013-06-05 22:08:47 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KioskServer::Application.config.secret_token = '44269c20be729c27f2cce07138f1d3117d0360cd748a144430bcd839445ec8549d61ec3c5f98ca6c2e1a3234dd470c498827f25ad16098eac21069d9c4164da8'
